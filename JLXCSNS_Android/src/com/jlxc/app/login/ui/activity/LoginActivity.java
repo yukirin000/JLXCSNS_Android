9bba60116f72779be17106d45d23f8c0a7095c27
yukirin <yukirin_000@163.com>
2015-07-06 09:45:27 +0800

    架构

---
+++ JLXCSNS_Android/src/com/jlxc/app/login/ui/activity/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.jlxc.app.login.ui.activity;
+
+import android.view.View;
+
+import com.jlxc.app.base.ui.activity.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+	@Override
+	public int setLayoutId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void loadLayout(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setUpView() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
