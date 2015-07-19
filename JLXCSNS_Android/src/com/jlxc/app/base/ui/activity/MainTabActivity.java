5538c03a52366c649fc9deef158df265a6ee7c63
Nicky_Liu <liufh8989@foxmail.com>
2015-07-19 10:35:25 +0800

    no message

--- JLXCSNS_Android/src/com/jlxc/app/base/ui/activity/MainTabActivity.java
+++ JLXCSNS_Android/src/com/jlxc/app/base/ui/activity/MainTabActivity.java
@@ -1,34 +1,19 @@
 package com.jlxc.app.base.ui.activity;
 
-import io.rong.imkit.RongIM;
-import io.rong.imlib.RongIMClient.ConnectCallback;
-import io.rong.imlib.RongIMClient.ErrorCode;
 
 import com.jlxc.app.R;
-import com.jlxc.app.base.helper.RongCloudEvent;
-import com.jlxc.app.base.manager.ActivityManager;
 import com.jlxc.app.base.manager.UserManager;
 import com.jlxc.app.base.model.UserModel;
 import com.jlxc.app.base.ui.activity.BaseActivity;
-import com.jlxc.app.base.utils.LogUtils;
-import com.jlxc.app.login.ui.activity.LoginActivity;
-import com.jlxc.app.message.ui.fragment.MessageMainFragment;
 import com.jlxc.app.news.ui.fragment.CampusFragment;
 import com.jlxc.app.news.ui.fragment.MainPageFragment;
 import com.jlxc.app.news.ui.fragment.NewsListFragment;
-import com.jlxc.app.personal.ui.activity.AccountSettingActivity;
 import com.jlxc.app.personal.ui.fragment.PersonalFragment;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Process;
 import android.support.v4.app.FragmentTabHost;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -51,7 +36,16 @@ public class MainTabActivity extends BaseActivity {
 			R.drawable.tab_message_btn };
 
 	private String mTextviewArray[] = { "主页", "消息", "我" };
-	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		initTab();
+		
+//		initRong();
+	}
+
 	public void initTab() {
 
 		layoutInflater = LayoutInflater.from(this);
@@ -69,33 +63,34 @@ public class MainTabActivity extends BaseActivity {
 		}
 	}
 	
-	//初始化融云
-	private void initRong(){
-		String token = "";
-		UserModel userModel = UserManager.getInstance().getUser();
-		if (null != userModel.getIm_token() && userModel.getIm_token().length()>0) {
-			token = userModel.getIm_token();
-		}
-		RongIM.connect(token, new ConnectCallback() {
-
-			@Override 
-			public void onError(ErrorCode arg0) {
-				Toast.makeText(MainTabActivity.this, "connect onError", Toast.LENGTH_SHORT).show();
-			}
-
-			@Override
-			public void onSuccess(String arg0) {
-				Toast.makeText(MainTabActivity.this, "connect onSuccess", Toast.LENGTH_SHORT).show();
-				RongCloudEvent.getInstance().setOtherListener();
-			}
-
-			@Override
-			public void onTokenIncorrect() {
-				Toast.makeText(MainTabActivity.this, "token error", Toast.LENGTH_SHORT).show();
-			}
-
-		});
-	}
+//	//初始化融云
+//	private void initRong(){
+//		String token = "";
+//		UserModel userModel = UserManager.getInstance().getUser();
+//		if (null != userModel.getIm_token() && userModel.getIm_token().length()>0) {
+//			token = userModel.getIm_token();
+//		}
+//		RongIM.connect(token, new ConnectCallback() {
+//
+//			@Override 
+//			public void onError(ErrorCode arg0) {
+//				Toast.makeText(MainTabActivity.this, "connect onError", Toast.LENGTH_SHORT).show();
+//			}
+//
+//			@Override
+//			public void onSuccess(String arg0) {
+//				Toast.makeText(MainTabActivity.this, "connect onSuccess", Toast.LENGTH_SHORT).show();
+//				RongCloudEvent.getInstance().setOtherListener();
+//			}
+//
+//			@Override
+//			public void onTokenIncorrect() {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//		});
+//	}
 
 	/**
 	 */
@@ -123,49 +118,6 @@ public class MainTabActivity extends BaseActivity {
 
 	@Override
 	protected void setUpView() {
-		initTab();
-		initRong();
-	}
-	
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-	}
-	
-	/**
-	 * 重写返回操作
-	 */
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-			final AlertDialog.Builder alterDialog = new AlertDialog.Builder(this);
-	        alterDialog.setMessage("确定退出该账号吗？");
-	        alterDialog.setCancelable(true);
 
-	        alterDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-	            @Override
-	            public void onClick(DialogInterface dialog, int which) {
-	                if (RongIM.getInstance() != null)
-	                    RongIM.getInstance().logout();
-	                ActivityManager.getInstence().exitApplication();
-	                killThisPackageIfRunning(MainTabActivity.this, "com.jlxc.app");
-	                Process.killProcess(Process.myPid());
-	            }
-	        });
-	        alterDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-	            @Override
-	            public void onClick(DialogInterface dialog, int which) {
-	                dialog.cancel();
-	            }
-	        });
-	        alterDialog.show();
-			return true;
-		} else
-			return super.onKeyDown(keyCode, event);
 	}
-	
-	public static void killThisPackageIfRunning(final Context context, String packageName) {
-        android.app.ActivityManager activityManager = (android.app.ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        activityManager.killBackgroundProcesses(packageName);
-    }
 }
