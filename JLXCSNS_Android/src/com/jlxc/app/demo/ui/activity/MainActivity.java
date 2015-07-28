c6b129b6cac7e36a3f83fca12c56ad02d7584c22
yukirin <yukirin_000@163.com>
2015-07-28 21:09:39 +0800

    冲突解决

--- JLXCSNS_Android/src/com/jlxc/app/demo/ui/activity/MainActivity.java
+++ JLXCSNS_Android/src/com/jlxc/app/demo/ui/activity/MainActivity.java
@@ -72,16 +72,16 @@ public class MainActivity extends BaseActivity {
 		// }
 		//
 		// }, null));
-		btnTest = (Button) findViewById(R.id.btn_test);
-		btnTest.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent();
-				intent.setClass(MainActivity.this, RegisterActivity.class);
-				startActivity(intent);
-			}
-		});
+//		btnTest = (Button) findViewById(R.id.);
+//		btnTest.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				Intent intent = new Intent();
+//				intent.setClass(MainActivity.this, RegisterActivity.class);
+//				startActivity(intent);
+//			}
+//		});
 	}
 
 	public void initTab() {
